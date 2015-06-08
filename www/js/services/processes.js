422eea3a6815802acab74365426b2c248c441d21
João Mello <joao.mello@uniriotec.br>
2015-06-07 23:34:31 -0300

    Apontando backend para heroku

--- www/js/services/processes.js
+++ www/js/services/processes.js
@@ -2,21 +2,34 @@ angular.module('starter.services', [])
 
 .factory('Processes', ['$http', '$filter', function ($http, $filter) {
   var factory = {};
+  var endpoint = "http://regra-clara-service.herokuapp.com/"
 
   factory.all = function () {
-    return $http.get("http://private-506c1-socialprocess.apiary-mock.com/processes");
+    return $http.get(endpoint + "procedures");
   };
 
   factory.get = function (processId) {
-    return $http.get("http://private-506c1-socialprocess.apiary-mock.com/processes/"+ processId);
+    return $http.get(endpoint + "procedures/"+ processId);
   };
 
   factory.getRule = function (processId, ruleId) {
-    return $http.get("http://private-506c1-socialprocess.apiary-mock.com/processes/"+ processId + '/rules/' + ruleId);
+    return $http.get(endpoint + "procedures/"+ processId + '/rules/' + ruleId + '/questions');
   };
 
   factory.getQuestion = function (processId, ruleId, questionId) {
-    return $http.get("http://private-506c1-socialprocess.apiary-mock.com/processes/"+ processId + '/rules/' + ruleId + "/questions/" + questionId);
+    return $http.get(endpoint + "procedures/"+ processId + '/rules/' + ruleId + "/questions/" + questionId + '/replies');
+  };
+
+  factory.setComment = function(processId, data) {
+    return $http.post(endpoint + "procedures/" + processId + "/comments", { comment: data })
+  };
+
+  factory.setRuleQuestion = function(processId, ruleId, data) {
+    return $http.post(endpoint + "procedures/"+ processId +"/rules/"+ ruleId +"/questions", { question: data })
+  };
+
+  factory.setRuleQuestionReply = function(processId, ruleId, questionId, data) {
+    return $http.post(endpoint + "procedures/"+processId+"/rules/"+ ruleId +"/questions/"+ questionId +"/replies", {reply: data})
   };
 
   return factory;
